bace217dbbc9a926cf7ada8613e3f2e53e1bd61e
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2020-12-06 05:17:03 +0000

    Add Minimal Day 6

---
+++ src/crystal/6.cr
@@ -0,0 +1,15 @@
+module AdventOfCode
+  extend self
+
+  def run_6_1
+    File.read("#{__DIR__}/input/6.txt").split("\n\n").sum do |group|
+      (group.chars.to_set - ['\n']).size
+    end
+  end
+
+  def run_6_2
+    File.read("#{__DIR__}/input/6.txt").split("\n\n").sum do |group|
+      group.split('\n').map(&.chars.to_set).reduce { |acc, set| acc & set }.size
+    end
+  end
+end
